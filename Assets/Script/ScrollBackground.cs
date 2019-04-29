69e0da38ba65afb7d456bd3f151dc20bdc5e4fbe
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-04-29 21:55:34 +0900

    床のスライドに成功

---
+++ Assets/Script/ScrollBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//======================================================================
+// 背景画像をスクロールさせるのを扱うクラス
+//======================================================================
+public class ScrollBackground : MonoBehaviour {
+	public float scrollSpeed = 1.0f;
+	public float scrollStartPoint;
+	public float scrollEndPoint;
+
+	void Update() {
+		// 毎フレームごとに、位置座標をずらす -> 背景がスクロールする
+		transform.Translate(-1 * scrollSpeed * Time.deltaTime, 0, 0);
+
+		if (transform.position.x <= scrollEndPoint) {
+			endPositionBackGround();
+		}
+	}
+
+	private void endPositionBackGround() {
+		transform.Translate(-1 * (scrollEndPoint - scrollStartPoint), 0, 0);
+
+		SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+	}
+
+}
