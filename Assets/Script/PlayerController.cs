67f506e3b81e4e9894bb832141647e36ee42312d
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-04-30 12:25:50 +0900

    お寿司が飛ぶようになった(地面を貫く不具合も修正) Groundにコライダーが設定してなかった

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody2D rigidBody;
+
+	public float maxHeightViewArea;
+	public float playerUpVector;
+
+	void Awake() {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Fire1") && transform.position.y < maxHeightViewArea) {
+			upVector();
+		}
+	}
+
+	public void upVector() {
+		rigidBody.velocity = new Vector2(0.0f, playerUpVector);
+	}
+
+}
