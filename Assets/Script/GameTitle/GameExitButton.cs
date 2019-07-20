28023a643565d887bc4d5b0b8e2a150e29dfdea8
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-07-20 18:18:33 +0900

    スタート画面にボタンの配置＆ゲーム画面遷移時にロード画面を追加＆Exitボタンの実装

---
+++ Assets/Script/GameTitle/GameExitButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameExitButton : MonoBehaviour {
+	public void ExitButtonTap() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#elif UNITY_STANDALONE
+			UnityEngine.Application.Quit();
+		#endif
+	}
+}
