c1d8fb645147aa825e021a68672d5feb0476fc0f
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-08-23 22:33:07 +0900

    画像の差し替えとエディタ拡張の沼に漬かり始める

---
+++ Assets/Editor/EditorExWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorExWindow : EditorWindow {
+	// menuのWindowにEditorExという項目を追加する
+	[MenuItem("Window/EditorEx")]
+	static void Open() {
+		// EditorExを選択するとOpen()が呼ばれる
+		// 表示させたいウィンドウは基本的にGetWindow()で表示＆取得する
+		EditorWindow.GetWindow<EditorExWindow>( "EditorWindow" );
+	}
+
+	// Windowクライアント領域のGUI処理
+	void OnGUI() {
+		EditorGUILayout.LabelField("Unityエディタ拡張");
+	}
+}
