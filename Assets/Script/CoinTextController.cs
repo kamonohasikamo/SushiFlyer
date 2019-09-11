70891a924fb8f5fbc51a0dd759d2d17ea764b101
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-09-11 21:42:07 +0900

    CoinのUI等の作成

---
+++ Assets/Script/CoinTextController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CoinTextController : MonoBehaviour
+{
+	public GameObject coinTextGameObject;
+	private Text coinText;
+
+	void Start() {
+		coinText = this.gameObject.GetComponent<Text>();
+		coinText.text = "×" + GameController.getGameScore().ToString("D3");
+	}
+
+	void Update() {
+		coinText.text = "×" + GameController.getGameScore().ToString("D3");
+	}
+}
